09f0e02751c37c8205848928542c1e82f27f9069
hana-bananaa <debartosiaki@gmail.com>
2021-08-05 13:38:21 -0300

    ajeitando side bar

--- src/sitebar/Sidebar.jsx
+++ src/sitebar/Sidebar.jsx
@@ -8,15 +8,51 @@ export default function sidebar() {
                     <img src="https://logo.com" alt="logo"/>
               </span>
           </div>
+
+
             <div className="sidebarItem">
-                <span className="aboutMe">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quaerat numquam, cum consequuntur ullam excepturi delectus eum molestias fugit odit iure quia.</p>
-                </span>
+                <p>lorem ipsum dolor sit amet, con lorem nib, lorem ipsum dolor sit amet</p>
             </div>
-            <div className="categories">
-                <span className="aboutMe">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quaerat numquam, cum consequuntur ullam excepturi delectus eum molestias fugit odit iure quia.</p>
+
+            <div className="sidebarItem">
+              <span className="sidebarTittle">
+                    <a href="https://dsdsd "><p>adrianvalentim@gmail.com</p></a>
+              </span>
+          </div>
+
+
+            <div className="sidebarItems">
+                <span className="sidebarItem">
+                        <p>Categories</p>
                 </span>
+                <ul className="sidebarList">    
+                    <ul className="sidebarItems">
+                        memes
+                        <ul className="sidebarItems">memes</ul>
+                        <ul className="sidebarItems">memes</ul>
+                        <ul className="sidebarItems">memes</ul>
+                    </ul>
+                    <ul className="sidebarItems">
+                        memes
+                        <ul className="sidebarItems">memes</ul>
+                        <ul className="sidebarItems">memes</ul>
+                        <ul className="sidebarItems">memes</ul>
+                    </ul>
+                    <ul className="sidebarItems">
+                        memes
+                        <ul className="sidebarItems">memes</ul>
+                        <ul className="sidebarItems">memes</ul>
+                        <ul className="sidebarItems">memes</ul>
+                    </ul>                
+                </ul>
+            </div>
+
+            <div className="sidebarItems">
+                <ul className="sidebarSocial">    
+                    <li className="sidebarItems">youtube</li>
+                    <li className="sidebarItems">linkedin</li>
+                    <li className="sidebarItems">GitHub</li>
+                </ul>
             </div>
         </div>
     )
